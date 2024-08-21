a640874deeda71030c4826749452024feab7b6cc
monstrasitix <dsavostijanovs@yahoo.ie>
2024-08-21 21:28:58 +0100

    Add Docker

--- apps/backend/src/api/v1/users/router.ts
+++ apps/backend/src/api/v1/users/router.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import * as userHandler from "@/api/v1/users/handler";
+import * as handler from "@/api/v1/users/handler";
 
 export default function () {
   const router = Router();
 
-  router.get("/users", userHandler.getUsers);
-  router.post("/users", userHandler.createUser);
-  router.get("/users/:id", userHandler.getUser);
-  router.delete("/users/:id", userHandler.deleteUser);
+  router.get("/users", handler.getUsers);
+  router.post("/users", handler.createUser);
+  router.get("/users/:id", handler.getUser);
+  router.delete("/users/:id", handler.deleteUser);
 
   return router;
 }
